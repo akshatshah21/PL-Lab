9056c5b03f4e07fbacb1be93be691c1d13b5879c
akshatshah21 <akshatshah_21@yahoo.in>
2019-10-15 23:49:23 +0530

    recursiveSum works

--- 1/recursiveSum.py
+++ 1/recursiveSum.py
@@ -1,34 +1,15 @@
-def recursiveSum(l, length):
-	sum = 0
-	if length == 1:
+def recursiveSum(l):
+	if len(l) == 0:
+		return 0;
+	if len(l) == 1:
 		return l[0]
-	if length == 2:
-		#print('Case len = 2')
-		return l[0] + l[1]
-	elif length != 0:
-		#print('Case len = ', length)
-		sum = l[-1] + recursiveSum(l[:-1], length - 1)
-		return sum
-	return sum
-		
-
-
-
-
-
-
-
-
-
+	else:
+		return l[-1] + recursiveSum(l[:-1])
 n = int(input('enter size: '))
-#l = [1] * 1000
 l = []
-#n = len(l)
-
-print('Enter elements one by one:')
-while n != 0:
-	l.append(int(input()))
-	n -= 1
-
-#print(l)
-print('Sum:', recursiveSum(l, n))
+if n!= 0:
+	print('Enter elements one by one:')
+	while n != 0:
+		l.append(int(input()))
+		n -= 1
+print('Sum:', recursiveSum(l))
